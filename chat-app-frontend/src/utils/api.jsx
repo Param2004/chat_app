627685ac871f91f08c1d5a3347555fdc0cd01543
Param sharma <kaushik.param2004@gmail.com>
2024-11-10 12:01:00 +0530

    deploy

--- chat-app-frontend/src/utils/api.jsx
+++ chat-app-frontend/src/utils/api.jsx
@@ -4,10 +4,10 @@ import axios from 'axios';
 export const fetchPendingRequests = async (username) => {
     try {
         const response = await axios.get(`https://sloth-mint-gnu.ngrok-free.app/api/users/rishabh/nearby`, { headers: {'ngrok-skip-browser-warning': 'true'} });
-        console.log(response.data);
+        // console.log(response.data);
         
         // Assuming the API returns an array of usernames with pending requests
-        return response;
+        return response.data;
     } catch (error) {
         console.error('Error fetching pending requests:', error);
         return [];
