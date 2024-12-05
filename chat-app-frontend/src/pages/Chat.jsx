623b381fd4f773bfffcec60c81a081f4a5e16cb4
Param sharma <kaushik.param2004@gmail.com>
2024-12-05 12:57:34 +0530

    test deploy #4

--- chat-app-frontend/src/pages/Chat.jsx
+++ chat-app-frontend/src/pages/Chat.jsx
@@ -52,11 +52,11 @@ const Chat = ({ ws }) => {
         }
     };
 
-    const name = String(otherUser[0]).toUpperCase() + String(otherUser).slice(1);
+    // const name = String(otherUser[0]).toUpperCase() + String(otherUser).slice(1);
 
     return (
         <div className="flex flex-col h-screen bg-gray-100 p-4">
-            <h2 className="text-2xl font-bold text-center mb-4">{name}</h2>
+            {/* <h2 className="text-2xl font-bold text-center mb-4">{name}</h2> */}
             <div className="flex-grow overflow-y-scroll p-4 space-y-2 bg-white rounded-md shadow-md">
                 {messages.map((msg, index) => (
                     <div
@@ -68,7 +68,7 @@ const Chat = ({ ws }) => {
                                 msg.sender === username ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
                             }`}
                         >
-                            <strong>{msg.sender === username ? "Me" : msg.sender}: </strong> {msg.content}
+                            <strong>{msg.sender === username ? "me" : msg.sender}: </strong> {msg.content}
                         </div>
                     </div>
                 ))}
